17f1e4725d4e73c1aa74623044f58ace1ee48454
Sebastian Celano <143465496+SebasCel@users.noreply.github.com>
2024-09-02 18:37:34 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
-// Variables globales
 let totalSimulaciones = 0;
 let escalerasEncontradas = 0;
 
-// Función para lanzar cinco dados
 function lanzarDados() {
     const dados = [];
     for (let i = 0; i < 5; i++) {
@@ -11,9 +9,7 @@ function lanzarDados() {
     return dados.sort((a, b) => a - b);
 }
 
-// Función para verificar si hay una escalera
 function esEscalera(dados) {
-    // Chequear si los números están en secuencia ascendente
     for (let i = 0; i < dados.length - 1; i++) {
         if (dados[i] + 1 !== dados[i + 1]) {
             return false;
@@ -22,7 +18,6 @@ function esEscalera(dados) {
     return true;
 }
 
-// Función para realizar simulaciones
 function realizarSimulaciones(cantidad) {
     for (let i = 0; i < cantidad; i++) {
         const resultado = lanzarDados();
@@ -35,7 +30,6 @@ function realizarSimulaciones(cantidad) {
     actualizarProbabilidad();
 }
 
-// Función para actualizar la interfaz con los resultados
 function actualizarResultados(resultado) {
     for (let i = 0; i < resultado.length; i++) {
         document.getElementById(`dado${i + 1}`).textContent = resultado[i];
@@ -43,14 +37,12 @@ function actualizarResultados(resultado) {
     document.getElementById('contador-simulaciones').textContent = totalSimulaciones;
 }
 
-// Función para actualizar la probabilidad en la interfaz
 function actualizarProbabilidad() {
     const probabilidad = (escalerasEncontradas / totalSimulaciones) * 100;
     document.getElementById('probabilidad-escalera').textContent = probabilidad.toFixed(2) + '%';
     actualizarGrafico();
 }
 
-// Función para actualizar la gráfica
 function actualizarGrafico() {
     const canvas = document.getElementById('grafico-probabilidad');
     const ctx = canvas.getContext('2d');
